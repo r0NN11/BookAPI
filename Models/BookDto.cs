aa111477d43c5a816370a8c92dc85ed01593f845
r0NN11 <dmitrykondratsky@gmail.com>
2025-02-23 23:14:17 +0400

    bookDto id restored

--- Models/BookDto.cs
+++ Models/BookDto.cs
@@ -2,6 +2,7 @@
 {
     public class BookDto
     {
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string AuthorName { get; set; } = string.Empty;
         public int PublicationYear { get; set; }
